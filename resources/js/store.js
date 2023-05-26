1385fbeff635697af78083d4094792f9bef80de6
atree-rohit <rohit.george@atree.org>
2023-05-26 12:32:02 +0530

    load mapfiles

--- resources/js/store.js
+++ resources/js/store.js
@@ -1,8 +1,16 @@
 import { createStore } from 'vuex'
+import districts from "./geojson/districts.json"
+import states from "./geojson/states.json"
+import regions from "./geojson/regions.json"
 
 const store = createStore({
     state: {
         test: 111,
+        geojson: {
+            districts: districts,
+            states: states,
+            regions: regions
+        }
     },
     mutations: {
     },
