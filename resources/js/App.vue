4de3c16992a1f7742e9867dd2b6dbc4f544b81b4
atree-rohit <rohit.george@atree.org>
2023-05-26 12:12:46 +0530

    vuex and setup

---
+++ resources/js/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        Store data {{ test }}
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue'
+    import {mapState} from 'vuex'
+
+    export default defineComponent({
+        name: "App",
+        computed:{
+            ...mapState(["test"])
+        },
+        mounted() {
+            console.log('App mounted')
+        },
+    })
+</script>
