6d772df248cbba0be6d0b21323af45a50b4ffc5a
atree-rohit <rohit.george@atree.org>
2023-05-29 17:16:57 +0530

    tweaks

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -13,6 +13,7 @@ class ImportController extends Controller
     public function import()
     {
         ini_set('max_execution_time', 300);
+        $existing_observation_ids = InatObservation::get()->pluck("id")->toArray();
         $fields = ["id", "observed_on", "user_id", "user_login", "user_name", "created_at", "updated_at", "quality_grade", "license", "image_url", "num_identification_agreements", "num_identification_disagreements", "oauth_application_id", "place_guess", "latitude", "longitude", "scientific_name", "common_name", "taxon_order_name", "taxon_superfamily_name", "taxon_family_name", "taxon_subfamily_name", "taxon_tribe_name", "taxon_subtribe_name", "taxon_genus_name", "taxon_species_name", "taxon_subspecies_name", "taxon_variety_name", "taxon_form_name"];
         $model_fields = [
             "observation" => ["id", "observed_on", "created_at", "updated_at", "quality_grade", "license", "image_url", "num_identification_agreements", "num_identification_disagreements", "oauth_application_id", "user_id"],
@@ -21,12 +22,18 @@ class ImportController extends Controller
             "location" => ["id", "place_guess", "latitude", "longitude", "region", "state", "district"]
         ];
         $model_names = ["observation", "user", "taxa", "location"];
-        $dir_files = scandir(public_path("/data/inat/expanded"));
-        $csv_filenames = array_filter($dir_files, function ($value) {
-            return pathinfo($value, PATHINFO_EXTENSION) === "csv";
-        });
-        $csv_filenames = ["2020_01_01_to_2021_06_01.csv"];
+        // $dir_files = scandir(public_path("/data/inat/expanded"));
+        // $csv_filenames = array_filter($dir_files, function ($value) {
+        //     return pathinfo($value, PATHINFO_EXTENSION) === "csv";
+        // });
+        $csv_filenames = [
+            "2021_01_01_to_2022_06_01.csv",
+            "2022_06_01_to_present.csv",
+            "upto_2020_01_01.csv"
+        ];
+        $op_interval = 10000;
         $add_counter = [];
+        ob_start();
         foreach($csv_filenames as $filename){
             $file = fopen(public_path("/data/inat/expanded/".$filename), "r");
             $header = fgetcsv($file);
@@ -51,59 +58,69 @@ class ImportController extends Controller
             ];
             
             while($row = fgetcsv($file)){
-                // continue;
                 $combined_row = array_combine($header, $row);
-                $model_data = [
-                    "user" => [
-                        "id" => $combined_row["user_id"],
-                        "login" => $combined_row["user_login"],
-                        "name" => $combined_row["user_name"]
-                    ],
-                    "taxa" => [
-                        "id" => $combined_row["taxon_id"],
-                        "scientific_name" => $combined_row["scientific_name"],
-                        "common_name" => $combined_row["common_name"],
-                        "order" => $combined_row["taxon_order_name"],
-                        "superfamily" => $combined_row["taxon_superfamily_name"],
-                        "family" => $combined_row["taxon_family_name"],
-                        "subfamily" => $combined_row["taxon_subfamily_name"],
-                        "tribe" => $combined_row["taxon_tribe_name"],
-                        "subtribe" => $combined_row["taxon_subtribe_name"],
-                        "genus" => $combined_row["taxon_genus_name"],
-                        "species" => $combined_row["taxon_species_name"],
-                        "subspecies" => $combined_row["taxon_subspecies_name"],
-                        "variety" => $combined_row["taxon_variety_name"],
-                        "form" => $combined_row["taxon_form_name"],
-                    ],
-                    "location" => [
-                        "place_guess" => $combined_row["place_guess"],
-                        "latitude" => $combined_row["latitude"],
-                        "longitude" => $combined_row["longitude"],
-                    ],
-                    "observation" => array_combine($model_fields["observation"], array_map(function($value) use ($combined_row){
-                        return $combined_row[$value];
-                    }, $model_fields["observation"]))
-                ];
-                $models = [
-                    "user" => InatUser::firstOrCreate($model_data["user"]),
-                    "taxa" => InatTaxa::firstOrCreate($model_data["taxa"]),
-                    "location" => InatLocation::firstOrCreate($model_data["location"]),
-                ];
-                $model_data["observation"]["taxa_id"] = $models["taxa"]->id;
-                $model_data["observation"]["location_id"] = $models["location"]->id;
-                // dd($model_data);
-                $models["observation"] = InatObservation::firstOrNew($model_data["observation"]);
-                
-                foreach($model_names as $mn){
-                    if($models[$mn]->wasRecentlyCreated){
-                        $add_counter[$filename][$mn]["created"]++;
-                    }else{
-                        $add_counter[$filename][$mn]["exists"]++;
-                    }
-
-                    if(($add_counter[$filename][$mn]["created"] > 10)|| ($add_counter[$filename][$mn]["exists"] > 10)){
-                        dd($add_counter);
-                    }
+                if(!in_array($combined_row["id"], $existing_observation_ids)){
+                    $model_data = [
+                        "user" => [
+                            "id" => $combined_row["user_id"],
+                            "login" => $combined_row["user_login"],
+                            "name" => $combined_row["user_name"]
+                        ],
+                        "taxa" => [
+                            "id" => $combined_row["taxon_id"],
+                            "scientific_name" => $combined_row["scientific_name"],
+                            "common_name" => $combined_row["common_name"],
+                            "order" => $combined_row["taxon_order_name"],
+                            "superfamily" => $combined_row["taxon_superfamily_name"],
+                            "family" => $combined_row["taxon_family_name"],
+                            "subfamily" => $combined_row["taxon_subfamily_name"],
+                            "tribe" => $combined_row["taxon_tribe_name"],
+                            "subtribe" => $combined_row["taxon_subtribe_name"],
+                            "genus" => $combined_row["taxon_genus_name"],
+                            "species" => $combined_row["taxon_species_name"],
+                            "subspecies" => $combined_row["taxon_subspecies_name"],
+                            "variety" => $combined_row["taxon_variety_name"],
+                            "form" => $combined_row["taxon_form_name"],
+                        ],
+                        "location" => [
+                            "place_guess" => $combined_row["place_guess"],
+                            "latitude" => $combined_row["latitude"],
+                            "longitude" => $combined_row["longitude"],
+                        ],
+                        "observation" => array_combine($model_fields["observation"], array_map(function($value) use ($combined_row){
+                            return $combined_row[$value];
+                        }, $model_fields["observation"]))
+                    ];
+                    $models = [
+                        "user" => InatUser::firstOrCreate($model_data["user"]),
+                        "taxa" => InatTaxa::firstOrCreate($model_data["taxa"]),
+                        "location" => InatLocation::firstOrCreate($model_data["location"]),
+                    ];
+                    $model_data["observation"]["taxa_id"] = $models["taxa"]->id;
+                    $model_data["observation"]["location_id"] = $models["location"]->id;
+                    // dd($model_data);
+                    $models["observation"] = InatObservation::firstOrCreate($model_data["observation"]);
+                    // $models["observation"]->save();
+                    
+                    foreach($model_names as $mn){
+                        if($models[$mn]->wasRecentlyCreated){
+                            $add_counter[$filename][$mn]["created"]++;
+                        }else{
+                            $add_counter[$filename][$mn]["exists"]++;
+                        }
+                    }   
+                } else {
+                    $add_counter[$filename]["observation"]["exists"]++;
+                }
+                if(($add_counter[$filename]["observation"]["created"] + $add_counter[$filename]["observation"]["exists"]) % ($op_interval/10) == 0 && ($add_counter[$filename]["observation"]["created"] + $add_counter[$filename]["observation"]["exists"]) > 0){
+                    echo $filename." > ".json_encode($add_counter[$filename]["observation"])."\n<br>";
+                    ob_flush();
+                }
+                if($add_counter[$filename]["observation"]["created"] >= $op_interval){
+                    echo "<pre>";
+                    print_r($add_counter);
+                    echo "</pre>";
+                    dd($add_counter[$filename]["observation"]["created"], $add_counter[$filename]["observation"]["exists"], $op_interval/10, ($add_counter[$filename]["observation"]["created"] + $add_counter[$filename]["observation"]["exists"]) % ($op_interval/10));
                 }
             }
             fclose($file);
