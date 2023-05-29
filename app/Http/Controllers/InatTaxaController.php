c472e3e476e30418095b731d4480ff4568b4c453
atree-rohit <rohit.george@atree.org>
2023-05-29 15:40:46 +0530

    other controllers

---
+++ app/Http/Controllers/InatTaxaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\InatTaxa;
+use Illuminate\Http\Request;
+
+class InatTaxaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(InatTaxa $inatTaxa)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(InatTaxa $inatTaxa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, InatTaxa $inatTaxa)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(InatTaxa $inatTaxa)
+    {
+        //
+    }
+}
