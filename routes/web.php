41953d1ceb32a6bf2c60d4593def86751d8728e1
atree-rohit <rohit.george@atree.org>
2023-06-23 15:23:51 +0530

    updated

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,12 @@ use App\Http\Controllers\ImportController;
 */
 
 Route::get('/', [HomeController::class, 'index']);
-Route::get('import', [ImportController::class, 'import']);
-Route::get('pull_data', [ImportController::class, 'pull_data']);
+Route::get('observations', [HomeController::class, 'observations']);
+Route::get('taxa', [HomeController::class, 'taxa']);
+Route::get('users', [HomeController::class, 'users']);
+Route::get('locations', [HomeController::class, 'locations']);
 
-Route::post('add_data', [ImportController::class, 'add_data']);
+// Route::get('import', [ImportController::class, 'import']);
+// Route::get('pull_data', [ImportController::class, 'pull_data']);
+
+// Route::post('add_data', [ImportController::class, 'add_data']);
