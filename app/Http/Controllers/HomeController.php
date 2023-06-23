b596f77c1b451f32d911f821d71f01e2b7f30d4e
atree-rohit <rohit.george@atree.org>
2023-06-23 15:24:26 +0530

    updated methods and api methods

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,10 +17,121 @@ use App\Models\InatLocation;
 class HomeController extends Controller
 {
     public function index(){
-        $data = InatObservation::where("nmw", '!=',null)->get()->groupBy("nmw");
-        dd($data["2012"][0]);
+        return view("welcome");
+        
+    }
+
+    public function observations(){
+        $data = InatObservation::get();
+        $unset_fields = ["latitude", "longitude", "quality_grade", "oauth_application_id", "num_identification_agreements", "num_identification_disagreements", "inat_created_at", "inat_updated_at"];
+        $clean_data = array_map(function($item) use (&$unset_fields){
+            $clean = $item;
+            $clean["created_at"] = date("Y-m-d", strtotime($item["inat_created_at"]));
+            $clean["updated_at"] = date("Y-m-d", strtotime($item["inat_updated_at"]));
+            foreach($unset_fields as $field){
+                unset($clean[$field]);
+            }
+            return array_values($clean);
+        }, $data->toArray());
+        return response()->json($clean_data);
+    }
+
+    public function taxa(){
+        $data = InatTaxa::select("id", "scientific_name as name", "common_name", "rank", "ancestry")->get()->keyBy("id");
+        return response()->json($data);
+    }
+
+    public function users(){
+        $data = InatUser::select("id", "login", "name")->get()->keyBy("id");
+        return response()->json($data);
+    }
+
+    public function locations(){
+        $data = InatLocation::select("id", "latitude", "longitude", "region", "state", "district")->get()->keyBy("id");
+        return response()->json($data);
+    }
+    
+    public function set_location(){
+        $data_loc = [];
+        $data = InatObservation::where("location_id", null)->get()->groupBy(function($item, int $key) use (&$data_loc){
+            $lat = number_format($item->latitude, 3, '.', '');
+            $lon = number_format($item->longitude, 3, '.', '');
+            $loc_key = "$lat,$lon";
+            if(!isset($data_loc[$loc_key])){
+                $data_loc[$loc_key] = [];
+            }
+            $data_loc[$loc_key][] = $item;
+        });
+        $districts = json_decode(file_get_contents(public_path('/data/districts.json')));
+        dd($data_loc);
+        foreach($data_loc as $k => $v){
+            $lat = explode(",", $k)[0];
+            $lon = explode(",", $k)[1];
+            $matching_district = null;
+            foreach($districts->features as $district){
+                foreach($district->geometry->coordinates as $polygon){
+                    $result = $this->isWithinBounds($lon, $lat, $polygon[0]);
+                    if($result){
+                        $matching_district = $district->properties;
+                    }
+                }
+            }
+            if($matching_district != null){
+                $update_ids = array_map(function($item){
+                    return $item->id;
+                }, $v);
+                $loc = new InatLocation();
+                $loc->latitude = $lat;
+                $loc->longitude = $lon;
+                $loc->district = $matching_district->district;
+                $loc->state = $matching_district->state;
+                $loc->region = $matching_district->region;
+                $loc->save();
+                InatObservation::whereIn("id", $update_ids)->update(["location_id" => $loc->id]);
+            }
+        }
 
     }
+
+    private function isWithinBounds($lon, $lat, $polygon) {
+        // dd($lon, $lat, $polygon);
+        $intersections = 0;
+    
+        $vertices = count($polygon);
+        for ($i = 0; $i < $vertices; $i++) {
+            $j = ($i + 1) % $vertices;
+    
+            if ($polygon[$i][1] == $polygon[$j][1]) {
+                continue;
+            }
+    
+            if ($lat < min($polygon[$i][1], $polygon[$j][1])) {
+                continue;
+            }
+    
+            if ($lat >= max($polygon[$i][1], $polygon[$j][1])) {
+                continue;
+            }
+    
+            $x = ($lat - $polygon[$i][1]) * ($polygon[$j][0] - $polygon[$i][0]) / ($polygon[$j][1] - $polygon[$i][1]) + $polygon[$i][0];
+    
+            if ($x > $lon) {
+                $intersections++;
+            }
+        }
+    
+        return ($intersections % 2) == 1;
+    }
+
+
+
+
+
+
+
+
+
+    // 
     public function index_add_nmw_year(){
         $data = InatObservation::where("nmw", null)->get()->groupBy("observed_on");
         // dd($data);
@@ -54,58 +165,7 @@ class HomeController extends Controller
         }
         
     }
-    public function index_3()
-    {
-        dd(1);
-        $d1 = "";
-        $d2 = "2011-11-01";
-        $params = [
-            "place_id" => 6681,
-            "taxon_id" => 47157,
-            "page" => 1,
-            "per_page" => 1,
-        ];
-        $completed_flag = false;
-        $total_results = 0;
-        $segments = [];        
-
-        while($completed_flag == false){
-            
-            do{
-                $params["d1"] = $d1;
-                $params["d2"] = $d2;
-                $data = $this->inatPull($params);
-                $total_results = +$data["total_results"];
-                if($data["total_results"] < 5000){
-                    $d2 = date("Y-m-d", strtotime($d2 . " + 1 months"));
-                } else if($data["total_results"] > 10000){
-                    $check = $d2;
-                    $d2 = date("Y-m-d", strtotime($d2 . " - 1 months"));
-                    if($check == $d2){
-                        dd($check, $d2);
-                    }
-                }
-            } while ($data["total_results"] < 5000 || $data["total_results"] > 10000) ;
-            $segments[] = [
-                "params" => [
-                    "d1" => $d1,
-                    "d2" => $d2,
-                ],
-                "pages" => ceil($data["total_results"] / 200),
-                "total_results" => $data["total_results"],
-            ];
-            if($data["total_results"] == 0 || $d2 > date("Y-m-d") || $total_results >= 50000){
-                $completed_flag = true;
-            }
-            else{
-                $d1 = $d2;
-                $d2 = date("Y-m-d", strtotime($d2 . " + 6 months"));
-            }
-            
-        }
-        dd($segments);
-    }
-
+    
     public function inatPull($params)
     {
         $response = Http::get('https://api.inaturalist.org/v1/observations', $params);
